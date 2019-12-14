1ac8f52e722951b04dd19c6e4592ea8e7a36862c
BRIGHTON Programmer <brighton.programmer@gmail.com>
2019-12-14 11:14:50 +0700

    Shop Front

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,11 @@
-<footer>
+<footer style="margin-top: 100px; margin-bottom: 20px; font-size: 15px;">
 
 <center>
-  <b>©Copyright - 2019 all rights reserved | <span style="color: #ff3535;"> Design & Developed With ❤ </span></b>
+  <b>©Copyright - 2019 all rights reserved
+    <br>
+    <br>
+    <span style="color: #ff3535;"> Design & Developed With ❤ </span>
+  </b>
 </center>
 
 </footer>
